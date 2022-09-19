0ad03809bd4928e284d513b3e3ca9563d93c3745
Jay Asole <jasole1111@gmail.com>
2022-09-19 20:08:23 +0530

    Mobile optimized navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import "./Navbar.css";
-import Logo from "../../assets/logo.jpg";
+import Logo from "../../assets/logo.png";
 import { CgMenu, CgCloseR } from "react-icons/cg";
 
 const Navbar = () => {
