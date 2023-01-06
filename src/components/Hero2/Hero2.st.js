4a774315697954f9b6dce6fb9d6685bcc062a239
Jitendra Asole <40131393+jay112298@users.noreply.github.com>
2023-01-06 20:00:38 +0530

    Add Hero2

---
+++ src/components/Hero2/Hero2.st.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Section = styled.div`
+    background: linear-gradient(90deg, #EFEEEE 0%, rgba(237, 236, 236, 0.885417) 36.98%, rgba(240, 240, 240, 0) 69.79%);;
+    display: flex;
+    align-items: center;
+    height: 900px;
+
+    .left {
+        flex: 1;
+        width: 100%;
+    }
+
+    .right{
+        height: 900px;
+        width: 100%;
+        flex: 2;
+        background: #EFB881;
+        border-bottom-left-radius: 60px;
+    }
+
+`
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -5,13 +5,11 @@ import "./Services.css"
 const Services = () => {
   return (
     <section className="section-about">
-      <img className="left" src="https://ik.imagekit.io/jsrsngp/vishnuji_kTc-SyQJC.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1665851769260" alt="vishnuji" />
+      <div className="left"></div>
       <div className="right">
         <div className="section-about-header"><h5>About Us</h5></div>
         <div className="section-about-header"><h2>JSRS</h2></div>
-        <div className="section-about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Ex pariatur nobis odio est praesentium sit aut quia quas?
-        Veniam cupiditate nulla vero, cum aspernatur quidem tenetur adipisci hic rem sequi.</div>
+        <div className="section-about-text">Om namo bhagvate vasudevaaya.</div>
       </div>
     </section>
   );
