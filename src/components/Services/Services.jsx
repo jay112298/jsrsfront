ec95d0d736c369d838551fc195e1660e41be27ce
Jitendra Asole <40131393+jay112298@users.noreply.github.com>
2023-01-09 21:44:46 +0530

    Create widget 1 outline

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -5,7 +5,25 @@ import { BodyBox } from "./Services.st";
 const Services = () => {
   return (
     <BodyBox>
-      <div className="widgets">Widgets</div>
+      <div className="widgets">
+        <div className="widget-box-1">
+          <div className="box1-top">Header</div>
+          <div className="box1-bottom">
+            Body
+            Body
+            Body
+            Body
+            Body
+          </div>
+        </div>
+        <div className="widget-box-2">Widget 2</div>
+        <div className="widget-box-3">Widget 3</div>
+        <div className="widget-box-4">Widget 4</div>
+        <div className="widget-box-5">Widget 5</div>
+        <div className="widget-box-6">Widget 6</div>
+        <div className="widget-box-7">Widget 7</div>
+        <div className="widget-box-8">Widget 8</div>
+      </div>
       <div className="sidebar">SideBar</div>
     </BodyBox>
   );
--- src/components/Services/Services.st.js
+++ src/components/Services/Services.st.js
@@ -7,7 +7,32 @@ export const BodyBox = styled.article`
         width: 100%;
         flex: 3;
         background: linear-gradient(to right, #edeceb, #efeeee, #efeeef, #f0efee, #f0f0f0);
+        display: grid;
+        align-items: center;
+        justify-content: center;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(3, 1fr);
+
+        .widget-box-1 {
+            .box1-top {
+                background-color: #feb86d;
+                margin: 15px;
+                border-radius: 5px;
+            }
+            .box1-bottom {
+                margin: 15px;
+                background-color: #feb86d;
+                border-radius: 5px;
+            }
+        }
+
+        .widget-box-4 {
+            grid-row: 2 / 4;
+        }
+
     }
+
+    
 
     .sidebar {
         padding-top: 50px;
