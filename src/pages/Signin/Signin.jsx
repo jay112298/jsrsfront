8c4d8b01ade737f19f64ed5f510cd03bab10b17f
Jitendra Asole <40131393+jay112298@users.noreply.github.com>
2023-01-09 19:51:44 +0530

    Remove imports from SignIn

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,10 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { Navigate } from "react-router-dom";
-import ramji from "../../assets/ramji.jpg";
-import "./Signin.css";
-import { isAuthenticated } from "../../../auth/helper/index";
-import { AiOutlineLoading3Quarters } from "react-icons/Ai";
-import { Button } from "@mui/material";
+import React from "react";
+
+
 
 const Signin = () => {
 
